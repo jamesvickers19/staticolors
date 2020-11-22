b9d24af904d7891efe207a785fbde2b9a194f7dc
James Vickers <jamesvickers19@gmail.com>
2020-11-22 10:50:03 -0700

    Converting app from React to vanilla JS - mostly working

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+const ctx = document.getElementById("canvas").getContext('2d');
+animLoop(() => paintCanvasWithRandomPixels(ctx));
+
+function paintCanvasWithRandomPixels(ctx) {
+  paintCanvasWithPixels(ctx, randomPixels(800 * 800));
+}
+
+// https://gist.github.com/louisremi/1114293#file_anim_loop_x.js
+function animLoop(renderFunc) {
+  var running, lastFrame = +new Date;
+  function loop(now) {
+      // stop the loop if render returned false
+      if (running !== false) {
+          requestAnimationFrame(loop);
+          var deltaT = now - lastFrame;
+          // do not render frame when deltaT is too high
+          if (deltaT < 160) {
+              running = renderFunc(deltaT);
+          }
+          lastFrame = now;
+      }
+  }
+  loop(lastFrame);
+}
+
+function paintCanvasWithPixels(ctx, pixelData) {
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+  ctx.clearRect(0, 0, width, height);
+  var newImageData = ctx.createImageData(width, height);
+  let i = 0;
+  for (const {red = 0, green = 0, blue = 0, alpha = 255} of pixelData) {
+    newImageData.data[i] = red;
+    newImageData.data[i + 1] = green;
+    newImageData.data[i + 2] = blue;
+    newImageData.data[i + 3] = alpha;
+    i += 4; 
+  }
+  ctx.putImageData(newImageData, 0, 0);
+}
+
+function randomPixels(length) {
+  let data = [];
+  for (var i = 0; i < length; i++) {
+    data.push({
+      red: randomInteger(0, 255),
+      //green: randomInteger(0, 255),
+      //blue: randomInteger(0, 255),
+      //alpha: randomInteger(0, 255)
+    });
+  }
+
+  return data;
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*
 
 class App extends React.Component {
   render() {
@@ -26,9 +89,7 @@ class Animation extends React.Component {
     this.updateAnimation();
   }
 
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+
   
   updateAnimationState() {
     this.setState(state => ({ imageData: this.props.getImageData(), stepMs: state.stepMs }));
@@ -98,22 +159,8 @@ class Canvas extends React.Component {
   }
 }
 
-function randomPixels(length) {
-  let data = [];
-  for (var i = 0; i < length; i++) {
-    data.push({
-      //red: randomInteger(0, 255),
-      green: randomInteger(0, 255),
-      //blue: randomInteger(0, 255),
-      //alpha: randomInteger(0, 255)
-    });
-  }
 
-  return data;
-}
-
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+*/
